5fa81fc712c6f5ac9d8f245fee7e6c57c67eadbc
puvanat <60070154@kmitl.ac.th>
2019-05-01 21:29:12 +0700

    Add change / shopedit / shopinfo

---
+++ content/shopinfo.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <!-- link css -->
+    <link rel="stylesheet" href="src/css/shopinfo.css">
+
+    <!-- link font -->
+    <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
+
+    <!-- link Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-nav">
+        <a class="navbar-brand" href="#">Home</a>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">				
+          </ul>
+        </div>
+        <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                    <a class="nav-link open-button" onclick="openForm()">REPORT</a>
+            </li>	
+            <li class="nav-item">
+                <a class="nav-link" href="#">SIGN UP</a>
+            </li>		
+            <li class="nav-item">
+                <a class="nav-link" href="?page=signin">SIGN IN</a>
+            </li>		
+            <li class="nav-item dropdown" style="float: right;">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                USER
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="left: -71% !important">
+                <a class="dropdown-item" href="#">Acount Setting</a>
+                <a class="dropdown-item" href="#">Shop Setting</a>
+                <a class="dropdown-item" href="#">Log Out</a>
+              </div>
+            </li>	
+          </ul>
+        </div>
+      </nav>
+      <div id='myNav' class="overlay">
+            <div class="form-popup" id="myForm">
+                <form action="/action_page.php" class="form-container">
+                <h1>Report</h1>
+                <button type="button" class="close bg-dark" aria-label="Close" onclick="closeForm()">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <textarea name="desc" id="desc" placeholder="Write here.." style="height:250px"></textarea>
+                <input type="submit">
+                </form>
+            </div>
+        </div>
+        <div class="box1">
+                <div class="container">
+                        <img id="expandedImg" style="width:100%">
+                </div>
+                <div class="row">
+                        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                        <div class="column2 mg1">
+                          <img src="manu.png" alt="Nature" style="width:100%" onclick="myFunction(this);">
+                        </div>
+                        <div class="column2">
+                          <img src="manu2.jpg" alt="Snow" style="width:100%" onclick="myFunction(this);">
+                        </div>
+                        <div class="column2">
+                          <img src="manu3.jpg" alt="Mountains" style="width:100%" onclick="myFunction(this);">
+                        </div>
+                        <a class="next" onclick="plusSlides(-1)">&#10095;</a>
+                      </div>
+        </div>
+        <div class=box2>
+            <div class="container2">
+                    <label for="shopnm"><b>Shop Name</b></label><br>
+                    <input type="text" name="shopnm"><br>
+                    <label for="Phone"><b>Phone</b></label><br>
+                    <input type="text" name="Phone"><br>
+            </div>
+            <div class="container3">
+                    <img src="manu.png" alt="">
+            </div>
+            <div class="abtshop">
+                    <label for="abtshop"><b>About Shop</b></label><br>
+                    <textarea name="desc" id="desc"  style="height:250px"></textarea>
+            </div>
+        </div>
+        <script>
+                function openForm() {
+                  document.getElementById("myForm").style.display = "block";
+                  document.getElementById("myNav").style.width = "100%";
+                }
+                function closeForm() {
+                  document.getElementById("myForm").style.display = "none";
+                  document.getElementById("myNav").style.width = "0%";
+                }
+                function myFunction(imgs) {
+                    var expandImg = document.getElementById("expandedImg");
+                    expandImg.src = imgs.src;
+                    expandImg.parentElement.style.display = "block";
+                }
+        </script>
+</body>
+</html>
