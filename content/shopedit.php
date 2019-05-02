5aeb28313e3991c28356318adde2e9a476ab84f6
puvanat <60070154@kmitl.ac.th>
2019-05-03 00:40:09 +0700

    adjust shopedit page from unexpected problem

--- content/shopedit.php
+++ content/shopedit.php
@@ -10,26 +10,27 @@
     <div class="nav2">
         <?php echo '<h1>' . $firstname . ' ' . $lastname . ' / ' . 'Profile Setting' . '</h1>'  ?>
     </div>
-    <div class="box">
-            <img src="manu.png" alt="" class='center'>
+    <div class="boxedit">
+            <img src="src/img/profile.jpg" alt="" class='center1'>
             <form action="">
-                    <label for="shopnm1"><b>Shop Name</b></label><br>
+                    <label class='shopnm15'>Shop Name</label><br>
                     <input type="text"  class='input1'><br>
-                    <label for="abshop1"><b>About Shop</b></label><br>
+                    <label  class='abshop15'>About Shop</label><br>
                     <textarea name="" id="" cols="10" rows="15" class='txtar1'></textarea><br>
                     <label for="promo" class='promo1'><b>Promotions</b></label><br>
-                    <div class="row">
                         <div class="column">
-                            <img src="manu.png" alt="promo1" style="width:100%">
+                            <img src="src/img/profile.jpg" class='arr' alt="promo1">
                         </div>
                         <div class="column">
-                                <img src="manu.png" alt="promo2" style="width:100%">
+                                <img src="src/img/palmdeath.jpg" class='arr' alt="promo2">
                         </div>
                         <div class="column">
-                                <img src="manu.png" alt="promo3" style="width:100%">
+                                <img src="src/img/profile.jpg" class='arr' alt="promo3">
                         </div>
-                    </div>
-                    <input type="submit">
+                    <input type="submit" class='submitinfo'>
             </form>
     </div>
+    <script>
+
+    </script>
 </body>
