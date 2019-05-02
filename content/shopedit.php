d527c16d815a71445612c0051df091d24cde87e5
puvanat <60070154@kmitl.ac.th>
2019-05-02 23:10:46 +0700

    adjust shopedit shopinfo add palmdeath palmwhite

--- content/shopedit.php
+++ content/shopedit.php
@@ -1,15 +1,23 @@
 <body>
+    <?php
+        $username = $_SESSION['username'];
+        $query = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username'");
+        $user_id = $result['user_id'];
+        $result = mysqli_fetch_assoc($query);
+        $firstname = $result['firstname'];
+        $lastname = $result['lastname'];
+    ?>
     <div class="nav2">
-        <h1>Steve Cartoon/Profile Setting</h1>
+        <?php echo '<h1>' . $firstname . ' ' . $lastname . ' / ' . 'Profile Setting' . '</h1>'  ?>
     </div>
     <div class="box">
             <img src="manu.png" alt="" class='center'>
             <form action="">
-                    <label for="shopnm"><b>Shop Name</b></label><br>
-                    <input type="text" name="shopnm"><br>
-                    <label for="abshop"><b>About Shop</b></label><br>
-                    <input type="text" name="abshop"><br>
-                    <label for="promo"><b>Promotions</b></label><br>
+                    <label for="shopnm1"><b>Shop Name</b></label><br>
+                    <input type="text"  class='input1'><br>
+                    <label for="abshop1"><b>About Shop</b></label><br>
+                    <textarea name="" id="" cols="10" rows="15" class='txtar1'></textarea><br>
+                    <label for="promo" class='promo1'><b>Promotions</b></label><br>
                     <div class="row">
                         <div class="column">
                             <img src="manu.png" alt="promo1" style="width:100%">
--- content/shopinfo.php
+++ content/shopinfo.php
@@ -1,36 +1,72 @@
 <body>
-    <div class="box1">
+    <div class="boxleft">
             <div class="container">
-                    <img id="expandedImg" style="width:100%">
+                    <div class='mySlides'>
+                        <img src="src/img/profile.jpg" class='picbig' style="width:100%">
+                    </div>
+                    <div class='mySlides'>
+                        <img src="src/img/palmdeath.jpg" class='picbig' style="width:100%">
+                    </div>
+                    <div class='mySlides'>
+                        <img src="src/img/palmgod.jpg" class='pigbig' style="width:100%">
+                    </div>
             </div>
             <div class="row">
-                    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-                    <div class="column2 mg1">
-                      <img src="manu.png" alt="Nature" style="width:100%" onclick="myFunction(this);">
+                    <a class="prev cursor" onclick="plusSlides(-1)">&#10094;</a>
+                    <div class="columnlft">
+                      <img src="src/img/profile.jpg" alt="manu1" class='demo cursor pic1' onclick="currentSlide(1)";>
                     </div>
-                    <div class="column2">
-                      <img src="manu2.jpg" alt="Snow" style="width:100%" onclick="myFunction(this);">
+                    <div class="columnlft">
+                      <img src="src/img/palmdeath.jpg" alt="manu2" class='demo cursor pic1' onclick="currentSlide(2)";>
                     </div>
-                    <div class="column2">
-                      <img src="manu3.jpg" alt="Mountains" style="width:100%" onclick="myFunction(this);">
+                    <div class="columnlft">
+                      <img src="src/img/palmgod.jpg" alt="manu3" class='demo cursor pic1' onclick="currentSlide(3)";>
                     </div>
-                    <a class="next" onclick="plusSlides(-1)">&#10095;</a>
+                    <a class="next cursor" onclick="plusSlides(1)">&#10095;</a>
                   </div>
-    </div>
-    <div class=box2>
-        <div class="container2">
-                <label for="shopnm"><b>Shop Name</b></label><br>
-                <input type="text" name="shopnm"><br>
-                <label for="Phone"><b>Phone</b></label><br>
-                <input type="text" name="Phone"><br>
         </div>
-        <div class="container3">
-                <img src="manu.png" alt="">
+        <div class=boxright>
+                <div class="container2">
+                        <label for="shopnm" class='shopinfonm'><b>Shop Name</b></label><br>
+                        <input type="text" name="shopnm" class='shopinfoinput'><br>
+                        <label for="Phone" class='shopinfophn'><b>Phone</b></label><br>
+                        <input type="text" name="Phone" class='inputphone'><br>
+                </div>
+                <div class="container3">
+                        <img src="src/img/profile.jpg" alt="">
+                </div>
+                <div class="infoabtshop">
+                        <label class="infoabtshoptxt"><b>About Shop</b></label><br>
+                        <textarea name="desc" id="infoarea" ></textarea>
+                </div>
         </div>
-        <div class="abtshop">
-                <label for="abtshop"><b>About Shop</b></label><br>
-                <textarea name="desc" id="desc"  style="height:250px"></textarea>
-        </div>
-    </div>
+        <script>
+                var slideIndex = 1;
+                        showSlides(slideIndex);
+
+                function plusSlides(n) {
+                        showSlides(slideIndex += n);
+                        }
+
+                function currentSlide(n) {
+                        showSlides(slideIndex = n);
+                }
+
+                function showSlides(n) {
+                        var i;
+                        var slides = document.getElementsByClassName("mySlides");
+                        var dots = document.getElementsByClassName("demo");
+                        if (n > slides.length) {slideIndex = 1}
+                        if (n < 1) {slideIndex = slides.length}
+                        for (i = 0; i < slides.length; i++) {
+                        slides[i].style.display = "none";
+                        }
+                        for (i = 0; i < dots.length; i++) {
+                        dots[i].className = dots[i].className.replace(" active", "");
+                        }
+                        slides[slideIndex-1].style.display = "block";
+                        dots[slideIndex-1].className += " active";
+                        }
+        </script>
 </body>
 </html>
